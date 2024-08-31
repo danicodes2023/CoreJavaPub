daa6d21427ccef38e4373412dbe9bed29b3ec4a7
danicodes2023 <danicodes2023@gmail.com>
2024-08-31 18:58:35 +0530

    String reversal

---
+++ src/com/interviewqs/StringReverse.java
@@ -0,0 +1,36 @@
+package com.interviewqs;
+
+import java.util.Arrays;
+
+/*Write a Java program to reverse a string.*/
+public class StringReverse {
+    public static void main(String[] args) {
+        String s = "AB";
+        System.out.println("Reversed string : " + reverseString(s));
+        System.out.println("Reversed string using reverseString2: " + reverseString2(s));
+    }
+
+    private static String reverseString(String s) {
+        char[] reverse = new char[s.length()];
+        for(int i = s.length()-1, j = 0; i >= 0 ; i--, j++) {
+            reverse[j] = s.charAt(i);
+        }
+        return new String(reverse);
+        //return Arrays.toString(reverse);
+    }
+    private static String reverseString2(String s) {// just another approach
+        int left = 0;
+        int right = s.length()-1;
+        char []charArray = s.toCharArray();
+        while(left < right){
+            char temp;
+            temp = s.charAt(left);
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            right--;
+            left++;
+        }
+        return new String(charArray);
+    }
+
+}
